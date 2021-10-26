6695494992b32f317d898fdb2565495dc24d6c90
arshita-s <arshita.237@gmail.com>
2021-10-26 16:53:30 -0700

    Can fill in log in fields - fields are validated in a basic manner

--- Source Code/FixitTicket/js-html-css/StudentLogIn.js
+++ Source Code/FixitTicket/js-html-css/StudentLogIn.js
@@ -1,11 +1,12 @@
-const loginUser = document.getElementById("login-form");
+const user = document.getElementById("username-field");
+const pass = document.getElementById("password-field")
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+    const username = user.value;
+    const password = pass.value;
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
