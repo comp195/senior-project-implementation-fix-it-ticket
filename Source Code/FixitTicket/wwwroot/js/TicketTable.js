a147f3b18a4c0615d9566ab0c3480eb5b7db32df
arshita-s <arshita.237@gmail.com>
2021-11-28 00:03:20 -0800

    Implemented update ticket front end page

--- Source Code/FixitTicket/wwwroot/js/TicketTable.js
+++ Source Code/FixitTicket/wwwroot/js/TicketTable.js
@@ -1,10 +1,13 @@
-const ticketsBody = document.querySelector(".paleBlueRows > tbody");
-document.addEventListener("DOMContentLoaded", () => {
-    loadTickets(); });
-document.addEventListener("load", () => {
-    loadTickets(); });
-const tempDataHolder = "http://localhost:8000/temp-data.json"
 
+let path = window.location.pathname;
+let page = path.split("/").pop();
+if(page == "resident_landing_page.html") {
+    document.addEventListener("DOMContentLoaded", () => {
+        loadTickets(); });
+    document.addEventListener("load", () => {
+        loadTickets(); });
+}
+const ticketsBody = document.querySelector(".paleBlueRows > tbody");
 function loadTickets() {
     const request = new XMLHttpRequest();
     request.open("GET", "api/Tickets");
@@ -24,7 +27,6 @@ function populateTickets(json) {
     while(ticketsBody.firstChild) {
         ticketsBody.removeChild(ticketsBody.firstChild);
     }
-    console.log(json);
     json.forEach((row) => {
         const tr = document.createElement("tr");
 
@@ -34,12 +36,14 @@ function populateTickets(json) {
         var status = document.createElement("td");
         var creationDate = document.createElement("td");
         var assignedId = document.createElement("td");
+        var comments = document.createElement("td");
         id.textContent = row.id;
         residentId.textContent = row.residentId;
         repairCategory.textContent = row.repairCategory;
         status.textContent = row.status;
+        comments.textContent = "Click to View";
         var dateDiff = Date.parse(row.creationDate);
-        var date = new Date(dateDiff);
+        var date = new Date(dateDiff).toLocaleDateString('en-US');
         creationDate.textContent = date;
         assignedId.textContent = row.assignedId ?? "";
         tr.appendChild(id);
@@ -48,7 +52,37 @@ function populateTickets(json) {
         tr.appendChild(status);
         tr.appendChild(creationDate);
         tr.appendChild(assignedId);
-
+        tr.appendChild(comments);
         ticketsBody.appendChild(tr);
     });
-} 
+    document.querySelector(".paleBlueRows tbody").addEventListener("click", function(event) {
+        var t = event.target;
+        if(t.textContent == "Click to View") {
+            console.log("BLAH BLAH")
+            return;
+        }
+        while (t !== this && !t.matches("tr")) {
+            t = t.parentNode;
+        }
+        if (t === this) {
+            console.log("No table cell found");
+        } else {
+            GrabUpdateTicket(t);
+        }
+    });
+    
+}
+
+function GrabUpdateTicket(row) {
+    var data = row.children;
+    
+    window.location.href = "/update_ticket.html?" + data[0].innerText;
+}
+
+
+
+
+
+
+    
+    
---
+++ Source Code/FixitTicket/wwwroot/js/UpdateTicket.js
@@ -0,0 +1,29 @@
+
+async function UpdateTicketStart() {
+    
+    var residentId = document.getElementById("resid");
+    const repairCategory = document.getElementById("Select_an_option");
+    const status = document.getElementById("Select_an_option_ew");
+    const assID = document.getElementById("assignedid")
+    const description = document.getElementById("description");
+    var ticketNum = document.getElementById("ticketNum");
+    let json = null;
+    let id = location.search.substring(1);
+    const request = new XMLHttpRequest();
+    request.open("GET", "api/Tickets/" + id);
+    request.onload = ()=>{
+        try {
+            json = JSON.parse(request.responseText);
+            ticketNum.textContent = json.id;
+            residentId.value = json.residentId;
+            repairCategory.textContent = json.repairCategory;
+            status.textContent = json.repairStatus;
+            assID.value = json.assignedId;
+            description.textContent = json.description;
+        }
+        catch(e) {
+            console.warn("Could not load ticket!");
+        }
+    };
+    request.send();
+}
