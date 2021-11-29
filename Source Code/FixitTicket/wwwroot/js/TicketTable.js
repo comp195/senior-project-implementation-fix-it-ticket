a733f03aa7f2ca2fee2425d62d9018f8107fef76
arshita-s <arshita.237@gmail.com>
2021-11-28 21:49:03 -0800

    Added functionality for users to sort tickets on the dashboard

--- Source Code/FixitTicket/wwwroot/js/TicketTable.js
+++ Source Code/FixitTicket/wwwroot/js/TicketTable.js
@@ -1,13 +1,75 @@
 
 let path = window.location.pathname;
 let page = path.split("/").pop();
-if(page == "resident_landing_page.html") {
-    document.addEventListener("DOMContentLoaded", () => {
-        loadTickets(); });
-    document.addEventListener("load", () => {
-        loadTickets(); });
-}
+document.addEventListener("DOMContentLoaded", () => {
+    loadTickets(); });
+document.addEventListener("load", () => {
+    loadTickets(); });
+
+
 const ticketsBody = document.querySelector(".paleBlueRows > tbody");
+const table = document.querySelector(".paleBlueRows");
+const headers = table.querySelectorAll('th');
+
+[].forEach.call(headers, function (header, index) {
+    header.addEventListener('click', function () {
+        sortColumn(index);
+    });
+});
+let tableBody = null;
+let rows = null;
+
+const sortColumn = function (index) {
+    // Get the current direction
+    const direction = directions[index] || 'asc';
+
+    // A factor based on the direction
+    const multiplier = (direction === 'asc') ? 1 : -1;
+    const newRows = Array.from(rows);
+    // Sort rows by the content of cells
+    newRows.sort(function (rowA, rowB) {
+        // Get the content of cells
+        const cellA = rowA.querySelectorAll('td')[index].innerHTML;
+        const cellB = rowB.querySelectorAll('td')[index].innerHTML;
+        const a = transform(index, cellA);
+        const b = transform(index, cellB);
+        switch (true) {
+            case a > b: return 1 * multiplier;
+            case a < b: return -1 * multiplier;
+            case a === b: return 0;
+        }
+    });
+
+    // Remove old rows
+    [].forEach.call(rows, function (row) {
+        tableBody.removeChild(row);
+    });
+
+    // Append new row
+    newRows.forEach(function (newRow) {
+        tableBody.appendChild(newRow);
+    });
+    directions[index] = direction === 'asc' ? 'desc' : 'asc';
+};
+
+const transform = function (index, content) {
+    const type = headers[index].getAttribute('data-type');
+    switch (type) {
+        case 'number':
+            return parseFloat(content);
+        case 'string':
+        default:
+            return content;
+    }
+};
+
+const directions = Array.from(headers).map(function (header) {
+    return '';
+});
+
+
+
+
 function loadTickets() {
     const request = new XMLHttpRequest();
     request.open("GET", "api/Tickets");
@@ -55,6 +117,8 @@ function populateTickets(json) {
         tr.appendChild(comments);
         ticketsBody.appendChild(tr);
     });
+    tableBody = table.querySelector('tbody');
+    rows = tableBody.querySelectorAll('tr');
     document.querySelector(".paleBlueRows tbody").addEventListener("click", function(event) {
         var t = event.target;
         if(t.textContent == "Click to View") {
